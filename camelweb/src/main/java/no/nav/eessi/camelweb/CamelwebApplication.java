59a809fcc499701df317ae5abbcbcfd76b2a781c
Arild Spikkeland <aril-spi@online.no>
2017-06-24 21:32:38 +0200

    Camel og web med Spring Boot

---
+++ camelweb/src/main/java/no/nav/eessi/camelweb/CamelwebApplication.java
@@ -0,0 +1,12 @@
+package no.nav.eessi.camelweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelwebApplication.class, args);
+	}
+}
